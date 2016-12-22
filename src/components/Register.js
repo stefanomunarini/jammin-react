f69751c7c42dc526f1c3e1063c5028e8df2d9b0e
Stefano <ssteee@gmail.com>
2016-12-22 17:53:38 +0200

    Remove logger

--- src/components/Register.js
+++ src/components/Register.js
@@ -64,8 +64,6 @@ class Register extends Component {
             // This gives you a Google Access Token. You can use it to access the Google API.
             let token = result.credential.accessToken;
 
-            console.log(result);
-
             let id = result.user.uid;
             let firstName = result.user.displayName.split(' ')[0];
             let lastName = result.user.displayName.split(' ')[1];
